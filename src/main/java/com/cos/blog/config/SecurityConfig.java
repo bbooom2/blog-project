f7efda18f3fe8dcdf5d5188f820dcf4b0f56ed8b
bbooom2 <ma22c0@naver.com>
2023-10-25 04:34:15 +0900

    2023-10-24

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -2,29 +2,33 @@ package com.cos.blog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration // IoC
-public class SecurityConfig {
+	@Configuration // IoC
+	public class SecurityConfig {
+	
+	 @Bean // IoC가 된다.
+	 BCryptPasswordEncoder encode() { // 시큐리티가 들고 있는 함수. 
+	  return new BCryptPasswordEncoder();
+	 }
 
- @Bean
- BCryptPasswordEncoder encode() {
-  return new BCryptPasswordEncoder();
- }
-
- @Bean
- SecurityFilterChain configure(HttpSecurity http) throws Exception {
-  http
-    .authorizeRequests()
-   		.antMatchers("/auth/**")
-   		.permitAll()
-   		.anyRequest()
-       .authenticated()
-    .and()
-       .formLogin()
-       .loginPage("/auth/loginForm");
-  return http.build();
- }
-}
+	 @Bean
+	 SecurityFilterChain configure(HttpSecurity http) throws Exception {
+	  http
+	  	.csrf().disable() // csrf 토큰 비활성화 (테스트시 걸어두는 게 좋음) 
+	    .authorizeRequests()
+	   		.antMatchers("/","/auth/**", "/js/**", "/css/**", "image/**")
+	   		.permitAll()
+	   		.anyRequest()
+	       .authenticated()
+	    .and()
+	       .formLogin()
+	       .loginPage("/auth/loginForm")
+	  	   .loginProcessingUrl("auth/loginProc") // 스프링 시큐리티가 해당 주소로 요청 오는 로그인을 가로채서 대신 로그인 해준다.
+	  	   .defaultSuccessUrl("/");
+	  return http.build();
+	 }
+	}

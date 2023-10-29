56424c400f7fac953b48451b6f6914c60e445fc2
bbooom2 <ma22c0@naver.com>
2023-10-29 22:53:36 +0900

    2023-10-29

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -2,10 +2,13 @@ let index = {
 	init: function() {
 		$("#btn-save").on("click", () => {
 			this.save();
-			});
+		});
+		$("#btn-delete").on("click", () => {
+			this.deleteById();
+		});
 	},
 	save: function() {
-		let data = { 
+		let data = {
 			title: $("#title").val(),
 			content: $("#content").val()
 		};
@@ -14,18 +17,36 @@ let index = {
 			type: "POST",
 			url: "/api/board",
 			data: JSON.stringify(data),
-			contentType: "application/json; charset=utf-8", 
-			dataType: "json" 
+			contentType: "application/json; charset=utf-8",
+			dataType: "json"
 
 		}).done(function(resp) {
 			alert("글쓰기가 완료되었습니다.");
 			console.log(resp);
-			location.href="/";
+			location.href = "/";
 
 		}).fail(function(error) {
 			alert(JSON.stringify(error));
 		});
-	}
+	},
+		deleteById: function() {
+			
+		var id = $("#id").text();
+
+		$.ajax({
+			type: "DELETE",
+			url: "/api/board/" + id,
+			dataType: "json"
+
+		}).done(function(resp) {
+			alert("삭제가 완료되었습니다.");
+			console.log(resp);
+			location.href = "/";
+
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		});
+	},
 }
 
 index.init();

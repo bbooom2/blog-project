e812d1d235954a450db7aac71e28026fb6eb1c96
bbooom2 <ma22c0@naver.com>
2023-10-30 02:01:43 +0900

    2023-10-30

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -27,4 +27,16 @@ public class UserService {
 		user.setRole(RoleType.USER);
 		userRepository.save(user);
 	}
+	
+	@Transactional
+	public void 회원수정(User user) { // 외부로부터 받은 user 
+		User persistance = userRepository.findById(user.getId()).orElseThrow(()-> {
+			return new IllegalArgumentException("회원 찾기 실패");
+		});
+		String rawPassword = user.getPassword();
+		String encPassword = encoder.encode(rawPassword);
+		persistance.setPassword(encPassword);
+		persistance.setEmail(user.getEmail());
+
+	}
 }
--- src/main/webapp/WEB-INF/views/user/joinForm.jsp
+++ src/main/webapp/WEB-INF/views/user/joinForm.jsp
@@ -15,10 +15,6 @@
 			<label for="email">Email</label> <input type="email" class="form-control" placeholder="Enter email" id="email">
 		</div>
 
-		<div class="form-group form-check">
-			<label class="form-check-label"> <input class="form-check-input" type="checkbox"> Remember me
-			</label>
-		</div>
 	</form>
 	<button id="btn-save" class="btn btn-primary">회원가입</button>
 </div>

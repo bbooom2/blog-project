0df1ebc4601d284f964267028c0b2cbe8fcb7621
bbooom2 <ma22c0@naver.com>
2023-10-30 19:43:34 +0900

    2023-10-30

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -1,7 +1,16 @@
 package com.cos.blog.controller;
 
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class UserController {
@@ -15,9 +24,44 @@ public class UserController {
 		return "user/loginForm";
 	}
 	
+	
+	@GetMapping("/auth/kakao/callback")
+	public @ResponseBody String kakaoCallback(String code) { // Data를 리턴해주는 컨트롤러 함수 
+		// POST 방식으로 key=value 데이터를 요청해야 한다. (카카오쪽으로) 
+		// a 태그를 활용하는 건 무조건 GET 방식 
+		// 이때 필요한 게 RestTemplate 
+		RestTemplate rt = new RestTemplate();
+		
+		//HttpHeader 오브젝트 생성 
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("ContentType", "application/x-www-form-urlencoded;charset=utf-8"); // 내가 전송한 바디 데이터가 키 밸류 형태의 데이터라는 것을 알려주는 것 
+		
+		// HttpBody 오브젝트 새성 
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "532b578db97f0e167c55932799b2cc3b");
+		params.add("redirect_uri", "http://localhost:8000/auth/kakao/callback");
+		params.add("code", code);
+		
+		// HttpHeader와 HttpBodey를 하나의 오브젝트에 담기 (exchange가 HttpEntity를 받게 되어 있기 때문)
+		HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
+				new HttpEntity<>(params, headers);
+		
+		// Http 요청하기 - Post 방식으로 - 그리고 response 변수의 응답 받음 
+		ResponseEntity<String> response = rt.exchange(
+				
+				"https://kauth.kakao.com/oauth/token",
+				HttpMethod.POST,
+				kakaoTokenRequest,
+				String.class
+		);
+		return "카카오 토큰 요청 완료 : 토큰 요청에 대한 응답 " + response;
+	}
+	
 	@GetMapping("/user/updateForm")
 	public String updateForm() {
 		return "user/updateForm";
 	}
-	
+
 }
